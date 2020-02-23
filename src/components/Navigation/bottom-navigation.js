5d12110550576ba32c7427062fefae292834ba92
Hari Irawan <hari31121993@gmail.com>
2020-02-23 15:33:49 +0700

    update bottom navigation

--- src/components/Navigation/bottom-navigation.js
+++ src/components/Navigation/bottom-navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import HomeIcon from "@material-ui/icons/Home";
@@ -9,7 +10,7 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 const useStyles = makeStyles({
   root: {
-    position: "absolute",
+    position: "fixed",
     bottom: 0,
     left: 0,
     right: 0
@@ -21,18 +22,24 @@ export default function Navigation(props) {
   const [value, setValue] = React.useState(0);
 
   return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      className={classes.root}
-      showLabels
-    >
-      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Feed" icon={<RssFeedIcon />} />
-      <BottomNavigationAction label="Cart" icon={<ShoppingBasketIcon />} />
-      <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />} />
-    </BottomNavigation>
+    <div className={classes.root}>
+      <Container maxWidth="xs">
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          showLabels
+        >
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+          <BottomNavigationAction label="Feed" icon={<RssFeedIcon />} />
+          <BottomNavigationAction label="Cart" icon={<ShoppingBasketIcon />} />
+          <BottomNavigationAction
+            label="Profile"
+            icon={<AccountCircleIcon />}
+          />
+        </BottomNavigation>
+      </Container>
+    </div>
   );
 }
