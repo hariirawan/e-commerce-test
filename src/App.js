c07dbfdaefd3c45fb2edd5d1620b764a27da330e
Hari Irawan <hari31121993@gmail.com>
2020-03-07 17:05:30 +0700

    update file app.js --> Menambahkan react-router-do

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
+import ProductDetail from "./views/ProductDetail";
+import SearchProduct from "./views/SearchProduct";
+import Login from "./views/Login";
+import Profile from "./views/Profile";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route path="/product-detail/:id?" component={ProductDetail} />
+        <Route path="/search" component={SearchProduct} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </Router>
   );
 }
 
