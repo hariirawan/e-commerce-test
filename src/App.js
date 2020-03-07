ef99ff7763eeae4ff433799c64baecbd0263d71b
Hari Irawan <hari31121993@gmail.com>
2020-03-07 20:27:47 +0700

    create protected routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import ProductDetail from "./views/ProductDetail";
 import SearchProduct from "./views/SearchProduct";
 import Login from "./views/Login";
 import Profile from "./views/Profile";
+import ProtectedRoutes from "./protected.routes";
+
+function PrivateRoute() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/product-detail/:id?" component={ProductDetail} />
+      <Route path="/search" component={SearchProduct} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route path="/product-detail/:id?" component={ProductDetail} />
-        <Route path="/search" component={SearchProduct} />
-        <Route path="/profile" component={Profile} />
+        <ProtectedRoutes path="/" component={PrivateRoute} />
       </Switch>
     </Router>
   );
---
+++ src/protected.routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function ProtectedRoutes({ component: Component, ...rest }) {
+  const login = useSelector(state => state.login);
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (login.status) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          );
+        }
+      }}
+    />
+  );
+}
